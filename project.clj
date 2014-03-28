dd9173006d4cd3229803be9335db0e98ee524606
Paul Umbers <paul.umbers@gmail.com>
2014-03-28 10:17:18 -0600

    Upgraded to Clojure 1.6.0

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/3rddog/doitnow"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
                  [compojure "1.1.6"]
                  [ring-middleware-format "0.3.2"]
                  [ring/ring-json "0.3.0"]
